fe7876e9c871a66ad21a4a538959b4ed76ce189e
Marc Steen Fordsmand <marc5517@edu.easj.dk>
2020-11-24 11:42:03 +0100

    Add project files.

---
+++ CoronaTestRest/Controllers/CoronaTestsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoronaTestRest.Model;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoronaTestRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoronaTestsController : ControllerBase
+    {
+        private static readonly List<CoronaTest> coronaTests = new List<CoronaTest>()
+        {
+            new CoronaTest(1, "Test maskine 1", 37.5, "Hovedindgangen", "24-11-2020", "11:37"),
+            new CoronaTest(2, "Test maskine 2", 37.2, "Bagdøren", "21-11-2020", "10:37"),
+            new CoronaTest(3, "Test maskine 3", 38.5, "Hovedindgangen", "4-12-2020", "14:43"),
+            new CoronaTest(4, "Test maskine 4", 39.1, "BR butikken", "29-11-2020", "17:15")
+        };
+
+        // GET: api/<CoronaTestsController>
+        [HttpGet]
+        public IEnumerable<CoronaTest> GetAllTests()
+        {
+            return coronaTests;
+        }
+
+        // GET api/<CoronaTestsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CoronaTestsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CoronaTestsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CoronaTestsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CoronaTestRest/Model/CoronaTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoronaTestRest.Model
+{
+    public class CoronaTest
+    {
+        private int _machineId;
+        private string _machineName;
+        private double _temperature;
+        private string _location;
+        private string _date;
+        private string _time;
+
+        public CoronaTest()
+        {
+
+        }
+
+        public CoronaTest(int machineId, string machineName, double temperature, string location, string date,
+            string time)
+        {
+            _machineId = machineId;
+            _machineName = machineName;
+            _temperature = temperature;
+            _location = location;
+            _date = date;
+            _time = time;
+        }
+
+        public int MachineId
+        {
+            get => _machineId;
+            set
+            {
+                _machineId = value;
+            }
+        }
+
+        public string MachineName
+        {
+            get => _machineName;
+            set
+            {
+                _machineName = value;
+            }
+        }
+
+        public double Temperature
+        {
+            get => _temperature;
+            set
+            {
+                _temperature = value;
+            }
+        }
+
+        public string Location
+        {
+            get => _location;
+            set
+            {
+                _location = value;
+            }
+        }
+
+        public string Date
+        {
+            get => _date;
+            set
+            {
+                _date = value;
+            }
+        }
+
+        public string Time
+        {
+            get => _time;
+            set
+            {
+                _time = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return
+                $"{nameof(MachineId)}: {_machineId}, {nameof(MachineName)}: {_machineName}, " +
+                $"{nameof(Temperature)}: {_temperature}, {nameof(Location)}: {_location}, " +
+                $"{nameof(Date)}: {_date}, {nameof(Time)}: {_time}";
+        }
+    }
+}
