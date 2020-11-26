e82019586521cb8238248f6c238fa6a4ef8b5fa5
Marc Steen Fordsmand <marc5517@edu.easj.dk>
2020-11-26 13:00:03 +0100

    Lavet en Unit Test på GET

---
+++ UnitTestCoronaTestRest/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using CoronaTestRest.Controllers;
+using CoronaTestRest.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestCoronaTestRest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private CoronaTestsController cntr = null;
+
+        [TestInitialize]
+        public void BeforeEachTest()
+        {
+            cntr = new CoronaTestsController();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<CoronaTest> liste = new List<CoronaTest>(cntr.Get());
+
+            Assert.AreEqual(8, liste.Count);
+        }
+    }
+}
